5e97eb73c3a70b685a080cf48b7a79c0a2056a56
pabloaguilarm <paguilar@heytrade.com>
2023-12-30 11:45:12 +0100

    Adding position state for circle

--- projects/03-mouse-follower/src/App.tsx
+++ projects/03-mouse-follower/src/App.tsx
@@ -1,27 +1,55 @@
 import { useEffect, useState } from 'react';
 
-function App() {
+type Position = {
+  x: number;
+  y: number;
+}
+
+const initialPosition: Position = { x: 0, y: 0 };
+
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState<boolean>(false);
+  const [position, setPosition] = useState<Position>(initialPosition);
   useEffect(() => {
-    console.log('effect', { enabled });
+    const handleMove = (event: { clientX: any; clientY: any; }) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+    // cleanup
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+      setPosition(initialPosition);
+    };
   }, [enabled]);
   return (
     <>
-      <div style={{
+      {enabled && <div style={{
         position: 'absolute',
         backgroundColor: '#09f',
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40
-      }}></div>
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}></div>}
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Deactivate' : 'Activate'} Track Pointer
       </button>
     </>
+  );
+};
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   );
 }
 
