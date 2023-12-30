2de4d00778e5979a78806101b512f49bc55a7f54
pabloaguilarm <paguilar@heytrade.com>
2023-12-30 11:05:43 +0100

    Adding circle to follow mouse

--- projects/03-mouse-follower/src/App.tsx
+++ projects/03-mouse-follower/src/App.tsx
@@ -7,6 +7,17 @@ function App() {
   }, [enabled]);
   return (
     <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40
+      }}></div>
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Deactivate' : 'Activate'} Track Pointer
       </button>
