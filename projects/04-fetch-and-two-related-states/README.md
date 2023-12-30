ca036c68d0f8ebfd689201315195ec687c01390a
pabloaguilarm <paguilar@heytrade.com>
2023-12-30 16:01:20 +0100

    Adding styles and logic

--- projects/04-fetch-and-two-related-states/README.md
+++ projects/04-fetch-and-two-related-states/README.md
@@ -3,8 +3,8 @@
 We have to do:
 
 1. Retrieve a random cat fact from the first API.
-2. Recover the first word of the fact.
-3. Show a picture of a cat with the first word.
+2. Recover the first three words of the fact.
+3. Show a picture of a cat with the first three words.
 
 ### APIs:
 
--- projects/04-fetch-and-two-related-states/src/App.tsx
+++ projects/04-fetch-and-two-related-states/src/App.tsx
@@ -1,11 +1,50 @@
+import './App.css'
+import { useEffect, useState } from 'react';
+
+type CatFactResponse = {
+  fact: string;
+  length: number;
+};
+
+type CatImageResponse = {
+  tags: string[];
+  _id: string;
+}
+
+const CAT_ENDPOINT_RANDOM_FACT: string = 'https://catfact.ninja/fact';
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
+
 function App() {
-
-
+  const [fact, setFact] = useState<string>();
+  const [pathImage, setPathImage] = useState<string>();
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then((data: CatFactResponse) => {
+        const { fact } = data;
+        setFact(fact);
+        const threeFirstWords = fact.split(' ', 3).join(' ');
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+          .then(res => res.json())
+          .then((data: CatImageResponse) => {
+            const { _id } = data;
+            setPathImage(`/cat/${_id}/says/${threeFirstWords}?fontSize=50&fontColor=red`);
+          });
+      });
+  }, []);
   return (
-    <>
-
-    </>
-  )
+    <main>
+      <h1>Kitty App</h1>
+      <button>Get new fact</button>
+      {fact && <p>{fact}</p>}
+      {pathImage &&
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${pathImage}`}
+          alt={`Image extracted using the first three words of ${fact}`}
+        />
+      }
+    </main>
+  );
 }
 
 export default App;
